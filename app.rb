21f70b5123c004ceeba1faa2ac5e01aa3ee2cb7a
Pamela Frieze <pfrieze@gmail.com>
2018-06-26 22:05:58 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -23,7 +23,7 @@ class App < Sinatra::Base
     @num1 = {params[:number1].to_i}
     @num2 = {params[:number2].to_i}
     @operation = {params[:operation]}
-    "#{@num1 @operation @num2"
+    "#{@num1 @operation @num2}"
   end
 
 end
